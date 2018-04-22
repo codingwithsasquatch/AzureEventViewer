5de856c1f5df58c456e63f012f4a323da0803367
Pete Roden <pete.roden@live.com>
2018-04-22 17:22:14 -0400

    static index.html and root proxy implemented

---
+++ IndexHtml.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.Net.Http.Headers;
+using System.IO;
+using System;
+
+namespace EventGeneratorAPI
+{
+    public static class IndexHtml
+    {
+        [FunctionName("IndexHtml")]
+        public static HttpResponseMessage Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "index.html")]HttpRequestMessage req, TraceWriter log)
+        {
+            FileStream stream;
+            if (Environment.GetEnvironmentVariable("isLocal") == "1")
+            {
+                stream = new FileStream(System.IO.Path.GetFullPath(@"www/index.html"), FileMode.Open, FileAccess.Read, FileShare.Read);
+            } else
+            {
+                stream = new FileStream(Path.Combine(System.Environment.GetEnvironmentVariable("HOME", EnvironmentVariableTarget.Process), @"site\wwwroot\www\index.html"), FileMode.Open, FileAccess.Read, FileShare.Read);
+            }
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StreamContent(stream);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return response;
+        }
+    }
+}
