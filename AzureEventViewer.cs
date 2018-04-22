a9d477162fc9f0b921ebc0acb5162c43165a0867
Pete Roden <pete.roden@live.com>
2018-04-22 11:44:32 -0400

    added queue insertion

--- AzureEventViewer.cs
+++ AzureEventViewer.cs
@@ -1,37 +1,48 @@
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Azure;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.EventGrid.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Polly;
 
 namespace AzureEventViewer
 {
     public static class AzureEventViewer
     {
         [FunctionName("AzureEventViewer")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             string requestContent = await req.Content.ReadAsStringAsync();
 
             //validate subscription if this is a validation request
             if (req.Headers.TryGetValues("Aeg-Event-Type", out IEnumerable<string> headerValues))
             {
-                string validationHeaderValue = headerValues.FirstOrDefault<string>();
-                if (validationHeaderValue == "SubscriptionValidation")
+                if (headerValues.FirstOrDefault<string>() == "SubscriptionValidation")
                 {
                     EventGridEvent[] events = JsonConvert.DeserializeObject<EventGridEvent[]>(requestContent);
-                    dynamic dataObject = events.FirstOrDefault().Data as dynamic;
-                    return req.CreateResponse(HttpStatusCode.OK, new { validationResponse = dataObject.validationCode });
+                    JObject dataObject = events.FirstOrDefault().Data as JObject;
+                    return req.CreateResponse(HttpStatusCode.OK, new { validationResponse = dataObject["validationCode"] });
                 }
             }
 
             log.Info(requestContent);
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("AzureWebJobsStorage"));
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference("EventQueue");
+            queue.CreateIfNotExists();
+            CloudQueueMessage message = new CloudQueueMessage(requestContent);
+            await queue.AddMessageAsync(message);
+
             return req.CreateResponse(HttpStatusCode.OK, requestContent);
         }
     }
