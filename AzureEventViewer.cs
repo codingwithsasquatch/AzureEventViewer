26aeec503d5b2759f6bd2cb920664ef380e514c5
David Barkol <dbarkol@hotmail.com>
2018-04-26 11:25:00 -0700

    Minor updates to header value validation. Added VS solution file.

--- AzureEventViewer.cs
+++ AzureEventViewer.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Linq;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.Azure;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
@@ -13,7 +11,6 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Polly;
 
 namespace AzureEventViewer
 {
@@ -22,36 +19,53 @@ namespace AzureEventViewer
         [FunctionName("AzureEventViewer")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
-            string requestContent = await req.Content.ReadAsStringAsync();
+            var requestContent = await req.Content.ReadAsStringAsync();
 
-            //validate subscription if this is a validation request
-            if (req.Headers.TryGetValues("Aeg-Event-Type", out IEnumerable<string> headerValues))
+            // Retrieve event type value from the header and return a bad
+            // request code if it is missing. 
+            if (!req.Headers.TryGetValues("Aeg-Event-Type", out var headerValues))
+                return req.CreateResponse(HttpStatusCode.BadRequest, "Missing event type in the header.");
+
+            var eventTypeHeaderValue = headerValues.FirstOrDefault();
+
+            // Echo back the validation code if the event type
+            // is for a subscription validation. 
+            if (eventTypeHeaderValue == "SubscriptionValidation")
             {
-                if (headerValues.FirstOrDefault<string>() == "SubscriptionValidation")
+                var events = JsonConvert.DeserializeObject<EventGridEvent[]>(requestContent);
+                if (events.FirstOrDefault()?.Data is JObject dataObject)
                 {
-                    EventGridEvent[] events = JsonConvert.DeserializeObject<EventGridEvent[]>(requestContent);
-                    JObject dataObject = events.FirstOrDefault().Data as JObject;
-                    return req.CreateResponse(HttpStatusCode.OK, new { validationResponse = dataObject["validationCode"] });
+                    return req.CreateResponse(HttpStatusCode.OK,
+                        new {validationResponse = dataObject["validationCode"]});
                 }
+
+                req.CreateErrorResponse(HttpStatusCode.BadRequest, "Invalid request");
+            }
+            else if (eventTypeHeaderValue == "Notification")
+            {
+                // Write the event to a storage queue if the event type is
+                // an event notification.
+                log.Info(requestContent);
+                try
+                {
+                    var storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("AzureWebJobsStorage"));
+                    var queueClient = storageAccount.CreateCloudQueueClient();
+                    var queue = queueClient.GetQueueReference("eventqueue");
+                    await queue.CreateIfNotExistsAsync();
+                    var message = new CloudQueueMessage(requestContent);
+                    await queue.AddMessageAsync(message);
+                }
+                catch (Exception e)
+                {
+                    log.Info(e.ToString());
+                    return req.CreateErrorResponse(HttpStatusCode.InternalServerError, "Error with storage queue");
+                }
+
+                return req.CreateResponse(HttpStatusCode.OK);
             }
 
-            log.Info(requestContent);
-            try
-            {
-                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("AzureWebJobsStorage"));
-                CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
-                CloudQueue queue = queueClient.GetQueueReference("eventqueue");
-                await queue.CreateIfNotExistsAsync();
-                CloudQueueMessage message = new CloudQueueMessage(requestContent);
-                await queue.AddMessageAsync(message);
-            }
-            catch (Exception e)
-            {
-                log.Info(e.ToString());
-                return req.CreateErrorResponse(HttpStatusCode.InternalServerError, "error with storage queue");
-            }
-
-                return req.CreateResponse(HttpStatusCode.OK, requestContent);
+            // Return a bad request if the event type is missing from the header.
+            return req.CreateResponse(HttpStatusCode.BadRequest, "Missing event type in the header.");
         }
     }
 }
